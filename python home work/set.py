c14c7927abeb866ef26b15c28f305e8cbd6f61e2
Rozaaaaaaa <149644598+Rozaaaaaaa@users.noreply.github.com>
2023-11-15 22:53:27 +0600

    Update set.py

--- python home work/set.py
+++ python home work/set.py
@@ -1,43 +1,3 @@
-#len-     барлық дубликатта жүреді      
-#in-      тексереді   
-#add-     жаңа элемент қосады , егер ол  SET ішінде болатын болса екінші рет қосылмайды ,себебі набордың ішінде екі элемент бірдей жүрмейді
-#update-  набордың ішіне бірнеше элемент қосу,massive ,tuple,set
-#pop-     рандомный элементті жояды набордан
-#isdisjoin-    екеуінде ортақ элементтер бар 
-
-
-
-# print(len(cities))
-
-# cities.add('Seoul')
-# print(len(cities))
-
-# new_city= input("Enter a city:  ")
-# cities.add(new_city)
-
-# print( cities)
-
-
-# new_city=input("Enter a  city: ")
-# if  new_city in cities:
-#     cities.remove(new_city)
-#     print(f"{new_city} was removed  from the set")
-# else:
-#     print("No such city")
-
-
-
-# cities={'Almaty','Rim','New-York','London','Aktobe'}
-
-# new_cities=input("Enter a  city: ")
-# print(new_cities)
-
-# cities.update(new_cities.split())
-# print(cities)
-
-
-
-
 
 #level 1
 
@@ -72,6 +32,7 @@ print(it_companies)
 
 
 #task 5     What is the difference between remove and discard
+
 #remove-элементтің тек біреуін өшіре аламыз
 #discard- массивтің ішін немесе массивтың өзін өшіреді
 
@@ -82,14 +43,54 @@ print(it_companies)
 #task 1
 A = {19, 22, 24, 20, 25, 26}
 B = {19, 22, 20, 25, 26, 24, 28, 27}
-B.isdisjoint(A)
-print(B)
-
+print(A.union(B))
 
 
 #task 2   Find A intersection B
+result = A.intersection(B)
+print(result)
 
-A.intersection(B)
-print(A)
+
+# #task 3
+result= A.issubset(B)
+print(result)
 
 
+#task 4  Are A and B disjoint sets
+print(A.isdisjoint(B))
+
+
+#task 5 Join A with B and B with A
+print(A.union(B))
+print(B.union(A))
+
+
+
+#task 6 What is the symmetric difference between A and B
+print(A.symmetric_difference(B))
+#{28,27} -its symmetric difference between A and B
+
+
+
+
+#task 7  Delete the sets completely
+del A
+del B
+
+
+
+#Level 3
+#task 1    Convert the ages to a set and compare the length of the list and the set, which one is bigger?
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+age=set(age)
+print(age)
+print(len(age))
+
+
+
+#task 2    Explain the difference between the following data types: string, list, tuple and set
+
+#string- өзгермейді,реттелген.
+#list- өзгермелі, ретімен жүреді, қайталанатын дубликаттар тізіміне жатады
+#tuple- элементтер өзгермейтін, қайталанатын дубликаттар тізіміне жатады
+#set- ретсіз,индектелмеген жіне өзгертілмейтін жинақ,жаңа элементтер қоса аламыз,қайталанатын  дубликаттар тізіміне жатпайды.
